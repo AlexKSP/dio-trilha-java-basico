bd409d0b611cc915192cbf0210b60b1444ecd87f
AlexKSP <lex.ksp@gmail.com>
2025-07-10 20:09:43 -0300

    Realizado exercicios proposto, cadastro de usuario

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        int acountNumber;
+        String agencia;
+        String name;
+        double balance;
+
+        System.out.println("----Login----");
+        System.out.println("Digite o número da conta: ");
+        acountNumber = sc.nextInt();
+        sc.nextLine();
+
+        System.out.println("Digite o número da Agência: ");
+        agencia = sc.nextLine();
+
+        System.out.println("Digite seu nome: ");
+        name = sc.nextLine();
+
+        System.out.println("Saldo atual: ");
+        balance = sc.nextDouble();
+
+        System.out.printf("%nOlá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque", name,agencia, acountNumber, balance);
+
+        sc.close();
     }
 }
